72e428e255ce51ca0f02c675343ffb9fa600643a
Brandon Scott <contact@brandonscott.me>
2025-01-02 16:07:02 -0500

    feat: add port, expiration days and connection id configuration options

--- pipegate/schemas.py
+++ pipegate/schemas.py
@@ -37,11 +37,14 @@ class BufferGateResponse(BufferGateCorrelationId):
 
 
 class JWTPayload(BaseModel):
-    sub: UUID4  # UUID of the user or connection_id
+    sub: str  # UUID of the user or connection_id
     exp: int  # Expiration timestamp
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(cli_parse_args=True)
+    expiration_days: int = Field(alias="PIPEGATE_EXPIRATION_DAYS")
+    connection_id: str = Field(alias="PIPEGATE_CONNECTION_ID")
+    port: int = Field(alias="PIPEGATE_PORT")
     jwt_secret: SecretStr = Field(alias="PIPEGATE_JWT_SECRET")
     jwt_algorithms: list[str] = Field(alias="PIPEGATE_JWT_ALGORITHMS")
--- pipegate/server.py
+++ pipegate/server.py
@@ -36,7 +36,7 @@ def get_settings(request: Request) -> Settings:
 
 
 def verify_jwt_uuid_match(
-    connection_id: UUID4,
+    connection_id: str,
     request: Request,
     settings: Settings = Depends(get_settings),
 ) -> JWTPayload:
@@ -112,7 +112,7 @@ def create_app() -> FastAPI:
         methods=list(get_args(Methods)),
     )
     async def handle_http_request(
-        connection_id: UUID4,
+        connection_id: str,
         request: Request,
         path_slug: str = "",
         payload: JWTPayload = Depends(verify_jwt_uuid_match),
@@ -121,7 +121,7 @@ def create_app() -> FastAPI:
         Handle incoming HTTP requests and forward them to the corresponding WebSocket connection.
 
         Args:
-            connection_id (uuid.UUID): The unique identifier for the connection.
+            connection_id (str): The unique identifier for the connection.
             request (Request): The incoming HTTP request.
             path_slug (str, optional): Additional path after the connection ID. Defaults to "".
 
@@ -171,14 +171,14 @@ def create_app() -> FastAPI:
 
     @app.websocket("/{connection_id}")
     async def handle_websocket(
-        connection_id: UUID4,
+        connection_id: str,
         websocket: WebSocket,
     ):
         """
         Manage WebSocket connections for sending and receiving data.
 
         Args:
-            connection_id (uuid.UUID): The unique identifier for the WebSocket connection.
+            connection_id (str): The unique identifier for the WebSocket connection.
             websocket (WebSocket): The WebSocket connection object.
         """
         await websocket.accept()
@@ -234,4 +234,6 @@ def create_app() -> FastAPI:
 
 if __name__ == "__main__":
     app = create_app()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    settings = Settings()
+    port = settings.port or 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
